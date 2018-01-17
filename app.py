4b3df69e7c386e28278883ef5f208ca93e9ffa71
Wayho <34697764+Wayho@users.noreply.github.com>
2018-01-17 15:18:46 +0800

    fix if code have chinese, run error

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 # Standard library imports
+import subprocess
 import codecs
 
 # External imports
@@ -21,6 +22,17 @@ sockets = Sockets(app)
 # flask_wtf
 WTF_CSRF_ENABLED = True
 app.config['SECRET_KEY'] = '592636cf2f301e0057b87375'
+
+STR_CODE_DEFAULT = \
+u"""
+# coding: utf-8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+########## 如要使用中文，请确保以上四行代码存在 ##########
+
+print '欢迎使用PyRun'
+"""
 
 class CommentForm(FlaskForm):
 	comment = TextAreaField("Comment", validators=[validators.DataRequired()])
@@ -52,25 +64,32 @@ def shell():
 	form = CommentForm()
 	lines = [u'http://man.linuxde.net/', u'ls -l', u'ifconfig', 'ps aux', 'more /usr/bin/start_lc_python.py']
 	datas = {'form': form, 'lines': lines}
+
 	if form.validate_on_submit():
 		lines = shellcmd(form.comment.data)
 		datas = {'form': form, 'lines': lines}
 	return render_template('shell.html', datas=datas)
 
+class PyRunForm(FlaskForm):
+	comment_code = TextAreaField("Code", validators=[validators.DataRequired()])
+	submit = SubmitField(u'确定')
+	comment_out = TextAreaField( "Output" )
+
 @app.route('/pyrun', methods=['GET', 'POST'])
 def pyrun():
-	form = CommentForm()
-	lines = [ u'Ready' ]
-	datas = {'form': form, 'lines': lines}
+	form = PyRunForm()
 	if form.validate_on_submit():
-		#print type(form.comment.data)
-		#print form.comment.data
-		Write_To_txt(form.comment.data)
-		lines = shellcmd( "python default.py" )
-		datas = {'form': form, 'lines': lines}
-	return render_template( 'pyrun.html', datas=datas )
+		#pass
+		Write_To_py(form.comment_code.data)
+		# return the output back to the python script
+		output = subprocess.Popen( [ "python default.py" ], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT )
+		output = output.communicate()[ 0 ]      #<type 'str'>
+		form.comment_out.data = output.decode('utf-8')
+	else:
+		form.comment_code.data = STR_CODE_DEFAULT
+	return render_template( 'pyrun.html', form=form )
 
-def Write_To_txt(text):
+def Write_To_py(text):
 	filepy = codecs.open("./default.py", "w", 'utf-8')
 	filepy.write(text)
 	filepy.close()
